cff761697a407828c6818af704a56f0c05a03b7c
Masda <akbmendoza@gbox.adnu.edu.ph>
2025-12-11 00:40:52 +0800

    Add interrogation_almostdone.cpp file

---
+++ Dispatch finals/interrogation_almostdone.cpp
@@ -0,0 +1,306 @@
+#include <iostream>
+#include <thread>//function for pause
+#include <chrono>// declaration of seconds and milliseconds
+using namespace std;
+
+void scene_apartment_interrogation(){
+    cout<<"Robert: Obviously, I love him. He's my Dad. "<<endl;
+    this_thread::sleep_for(chrono::seconds(3));// delay command for each outputs
+    cout<<"Soothing Voice: Hmm..."<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    cout<<"Robert: What does that hmm mean? Just say what you wanna say? "<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    cout<<"Soothing voice: It's just, When you add obviously it feels a little less obvious. "<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    cout<<"Soothing Voice: So, I'll ask again..."<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    cout<<"Soothing Voice: How do you really feel about your father? "<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    char a, w;
+    cout<<"A = I love my dad."<<endl;
+   
+    cout<<"W = Didn't know him well."<<endl;
+    
+    cout<<"D = He's gone"<<endl;
+    cout<<" ";
+    cin>>a;
+
+     if (a == 'A' || a =='a'){
+        cout<<"Soothing voice: It's as if, maybe, It's a little more complicated than you feel comfortable admitting.. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<" Which is completely understandable."<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Robert: look, I love my dad, alright? I love my dad. That's the truth. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<"Soothing voice: loved "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Robert: That's what i said. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Soothing voice: you said loved... "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<" Your dad's dead, so you don't love him, you loved him. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Robert: why would i be trying to find the man who killed him if I didn't love him? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Why would we be in any of this— this— "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Nose starts to bleed*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: shit. My nose is bleeding. Thought i was getting emotionally snotty."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: No, it's probably from when i kicked you in the face.  "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: yeah. Thanks for reminding me. So Here's gonna happen. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" I'm going to hang you off the side of this building, and you're gonna tell me where shroud is, or "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<" i'm going to drop you to your death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Come on, I thought we were having a breakthrough. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: Hey, we did. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Ya know, for being a real piece of shit, you're pretty easy to talk to. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<" But the only breakthrough I need right now, is the location of shroud. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<"Soothing voice: I'm not telling you shit you fuckin' loser. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: See. This is more like, how I thought this would go..."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: I hope shroud kills you just like he killed your father"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" I hope he fuckin' spanks your little daddy issue bitch ass to death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Are we outside? "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" It smells like what outside smells"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Threatens to drop him*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Fuckfuckfuck- wait! Wait! Wait! Wait"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey! Hey! I have neighbors"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Use your inside voice and just tell me where shroud is? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Okay, okay, uh uh- Steel- Steel Mill. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Threatens to drop him again*"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Llewelyn uh, Llewelyn Steel Works okay? Fuck. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: You're sure about that? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Yes! Please, that's all i know! "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Stop screaming, goddamnit"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey, hey, I will pull you up if you quiet the fuck down. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+    }
+     else if(a == 'W' || a == 'w'){
+        cout<<"Soothing voice: It's as if, maybe, It's a little more complicated than you feel comfortable admitting.. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<" Which is completely understandable."<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<"Robert: Didn't really know him well... he wasn't around much"<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Soothing voice: Well, you're gonna see him even less now, cause he's dead. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+                 cout<<" Robert: Why would i be trying to find the man who killed him if I didn't love him? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Why would we be in any of this- this- "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Nose starts to bleed*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: shit. My nose is bleeding. Thought i was getting emotionally snotty."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: No, it's probably from when i kicked you in the face.  "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: yeah. Thanks for reminding me. So Here's gonna happen. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" I'm going to hang you off the side of this building, and you're gonna tell me where shroud is, or "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<" i'm going to drop you to your death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Come on, I thought we were having a breakthrough. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: Hey, we did. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Ya know, for being a real piece of shit, you're pretty easy to talk to. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<" But the only breakthrough I need right now, is the location of shroud. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<"Soothing voice: I'm not telling you shit you fuckin' loser. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: See. This is more like, how I thought this would go..."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: I hope shroud kills you just like he killed your father"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" I hope he fuckin' spanks your little daddy issue bitch ass to death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Are we outside? "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" It smells like what outside smells"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Threatens to drop him*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Fuckfuckfuck- wait! Wait! Wait! Wait"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey! Hey! I have neighbors"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Use your inside voice and just tell me where shroud is? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Okay, okay, uh uh- Steel- Steel Mill. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Threatens to drop him again*"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Llewelyn uh, Llewelyn Steel Works okay? Fuck. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: You're sure about that? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Yes! Please, that's all i know! "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Stop screaming, goddamnit"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey, hey, I will pull you up if you quiet the fuck down. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+     }
+     else if(a == 'D'|| a == 'd'){
+        cout<<"Soothing voice: It's as if, maybe, It's a little more complicated than you feel comfortable admitting.. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<" Which is completely understandable."<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Robert: Look... he's gone. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<"Soothing voice: That's a bit of an understatement... "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+        cout<<"Robert: How so? "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<"Soothing voice: well, he's more than gone, cause he's fuckin' dead. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+        cout<<" That's the most gone you can be. "<<endl;
+        this_thread::sleep_for(chrono::seconds(4));
+         cout<<"Robert: just so sensitive. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Listen, if i didn't love him, we wouldn't be here, right? "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: we don't have to get into it. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: nope we're in it now. "<<endl;
+        this_thread::sleep_for(chrono::seconds(3));
+                 cout<<" why would i be trying to find the man who killed him if I didn't love him? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Why would we be in any of this— this— "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Nose starts to bleed*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: shit. My nose is bleeding. Thought i was getting emotionally snotty."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: No, it's probably from when i kicked you in the face.  "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: yeah. Thanks for reminding me. So Here's gonna happen. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" I'm going to hang you off the side of this building, and you're gonna tell me where shroud is, or "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<" i'm going to drop you to your death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Come on, I thought we were having a breakthrough. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: Hey, we did. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Ya know, for being a real piece of shit, you're pretty easy to talk to. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<"But the only breakthrough I need right now, is the location of shroud. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<"Soothing voice: I'm not telling you shit you fuckin' loser. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Robert: See. This is more like, how I thought this would go..."<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: I hope shroud kills you just like he killed your father"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" I hope he fuckin' spanks your little daddy issue bitch ass to death. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Are we outside? "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" It smells like what outside smells"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Threatens to drop him*"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Fuckfuckfuck- wait! Wait! Wait! Wait"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey! Hey! I have neighbors"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<" Use your inside voice and just tell me where shroud is? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Okay, okay, uh uh- Steel- Steel Mill. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Threatens to drop him again*"<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<" Llewelyn uh, Llewelyn Steel Works okay? Fuck. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: You're sure about that? "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: Yes! Please, that's all i know! "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Stop screaming, goddamnit"<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+         cout<<"Soothing voice: HELP! HEEEL-MMM "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+         cout<<"Robert: Hey, hey, I will pull you up if you quiet the fuck down. "<<endl;
+         this_thread::sleep_for(chrono::seconds(3));
+        
+
+     }
+    cout<<" A = let him drop"<<endl;
+    cout<<" D = Pull him Back"<<endl;
+    cin>> w;
+
+    if(w == 'a'|| w == 'A'){
+              cout<<"Robert: Sir. If you want something to scream about, I'll give you something to scream about"<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+         cout<<"Drops him*"<<endl;
+          cout<<" "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+                  cout<<"! He will remember that. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+    }
+    else if(w == 'd'|| w == 'D'){
+       cout<<"Robert: honestly it was nice talking to someone about this stuff. "<<endl;
+         this_thread::sleep_for(chrono::seconds(4));
+                   cout<<"pulls him back* "<<endl;
+                   cout<<" "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+                  cout<<"! He will remember that. "<<endl;
+         this_thread::sleep_for(chrono::seconds(2));
+    }
+    
+
+
+}
+
+int main() {
+  /*  std::cout << "First message" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));  // Pause for 2 seconds
+
+    std::cout << "Second message after pause" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));  // Pause for 500 milliseconds
+
+    std::cout << "Third message after another pause" << std::endl;
+*/
+  
+  scene_apartment_interrogation();
+    return 0;
+}
+
